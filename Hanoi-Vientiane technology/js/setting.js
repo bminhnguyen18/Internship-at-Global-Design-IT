f063657fea18c34579d614df10b7706eedd5d376
bminhnguyen18 <62327741+bminhnguyen18@users.noreply.github.com>
2020-06-22 16:58:50 +0700

    update photoslide

---
+++ Hanoi-Vientiane technology/js/setting.js
@@ -0,0 +1,11 @@
+(function($){
+    $('.main_slider .slider_wrap').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      });
+})(jQuery);
